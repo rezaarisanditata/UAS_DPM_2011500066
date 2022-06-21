a1adda121571413fb17b08d509f4905f667fd251
rezaarisanditata <arisandizn@gmail.com>
2022-06-21 21:25:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uas_dpm_2011500066/DataDosen.kt
@@ -0,0 +1,11 @@
+package com.example.uas_dpm_2011500066
+
+class DataDosen(
+    var Nidn: String,
+    var NmDosen: String,
+    var jabatan: String,
+    var GolPangkat: String,
+    var pendidikan: String,
+    var keahlian: String,
+    var ProgramStudi:String
+)
